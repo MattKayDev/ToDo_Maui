287e198297f8ac9e46e9caa0f7fe30319d5c42e7
Maciej Kolacinski <maciek200895@gmail.com>
2023-02-18 02:20:53 +0000

    Dev (#3)
    * First commit
    
    * Added constant to hold Database path as well as added a class to hold methods for database manipulation
    
    * Added empty views and binded them to matching view models
    In the view models started creating methods for calls to database

---
+++ ToDoMaui/ViewModels/TodoItemPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoMaui.Models;
+
+namespace ToDoMaui.ViewModels
+{
+    [INotifyPropertyChanged]
+    public partial class TodoItemPageViewModel
+    {
+        private TodoItemDatabase database;
+
+        [ObservableProperty]
+        private TodoItem item;
+        public TodoItemPageViewModel(TodoItemDatabase todoItemDatabase)
+        {
+            // pass across database and if null create a new one
+            database = todoItemDatabase ?? new TodoItemDatabase();
+        }
+
+        [RelayCommand]
+        private async void SaveItem()
+        {
+            if (string.IsNullOrWhiteSpace(Item.Title))
+            {
+                await AppShell.Current.DisplayAlert("Title required", "Please enter a title for the to do item", "OK");
+                return;
+            }
+
+            await this.database.SaveItemAsync(Item);
+            await Shell.Current.Navigation.PopToRootAsync();
+        }
+    }
+}
